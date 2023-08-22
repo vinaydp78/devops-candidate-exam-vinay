b7669969c4362c915ccf93790f7cef55a25118e7
Patange <vinay.patange.ext@siemens.com>
2023-08-22 22:28:21 +0530

    vinay2

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import json
 #import requests
-import http.client
+import urllib.request
 
 def lambda_handler(event, context):
     url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
@@ -11,21 +11,21 @@ def lambda_handler(event, context):
     "email": "vinaydp78@gmail.com"
     } # replace with your request parameters
     
-    #response = urllib.request.post(url, headers=headers, data=json.dumps(payload))
-   # return {
-    #    "statusCode": response.status_code,
-    #    "body": response.json()
-    #}
-    
-    conn = http.client.HTTPSConnection("https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data")
-    conn.request("POST", "/endpoint", body=payload, headers=headers)
-    
-    response = conn.getresponse()
-    response_data = response.read().decode("utf-8")
-    
-    # Return the response data
+    response = urllib.request.post(url, headers=headers, data=json.dumps(payload))
     return {
-        "statusCode": response.status,
+        "statusCode": response.status_code,
         "body": response.json()
     }
+    
+    #conn = http.client.HTTPSConnection("https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data")
+    #conn.request("POST", "/endpoint", body=payload, headers=headers)
+    
+    #response = conn.getresponse()
+    #response_data = response.read().decode("utf-8")
+    
+    # Return the response data
+    #return {
+    #    "statusCode": response.status,
+     #   "body": response.json()
+    #}
     
