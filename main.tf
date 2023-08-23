601526b699a58883397a074a06a58cbaca187162
Patange <vinay.patange.ext@siemens.com>
2023-08-23 09:48:20 +0530

    function update

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_route_table" "private_route_table" {
 
 # Create Lambda function
 resource "aws_lambda_function" "lambda_function" {
-  function_name = "my-lambda-function_vinay"
+  function_name = "my-lambda-function_vinay1"
   handler = "api.lambda_handler"
   runtime = "python3.7"
   role = data.aws_iam_role.lambda.arn
